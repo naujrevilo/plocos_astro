532e7237a72d5c04eff7b948ba7dbd06e8e8c00d
Nauj <14898430+naujrevilo@users.noreply.github.com>
2025-11-19 17:27:36 -0500

    feat(algolia): implementa búsqueda y variables de entorno

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,7 +4,7 @@
       <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
     </button>
     <div v-if="isSearchVisible" class="absolute top-full right-0 mt-2 w-80 p-4 bg-white rounded-lg border border-gray-200 shadow-lg dark:bg-gray-800 dark:border-gray-700 z-50">
-      <ais-instant-search :search-client="searchClient" index-name="plocos_netlify_app_gk0a52iptl_pages">
+      <ais-instant-search :search-client="searchClient" :index-name="indexName">
         <ais-search-box />
         <ais-hits>
           <template v-slot:item="{ item }">
@@ -33,7 +33,7 @@ export default {
         import.meta.env.PUBLIC_ALGOLIA_APP_ID,
         import.meta.env.PUBLIC_ALGOLIA_SEARCH_API_KEY
       ),
-      
+      indexName: import.meta.env.PUBLIC_ALGOLIA_INDEX_NAME,
       isSearchVisible: false,
     };
   },
