2722d8c9b68527dd605910fe2319d95ac6295166
Nauj <14898430+naujrevilo@users.noreply.github.com>
2025-11-22 15:12:22 -0500

    fix: revert output to server, keep netlify adapter fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://plocos.netlify.app',
-  output: 'hybrid',
+  output: 'server',
   adapter: netlify(),
 
   integrations: [
